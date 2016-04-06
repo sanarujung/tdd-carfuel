4fd2c303524908ed229546c660d7f243a0cdb54f
Watuka Thuannimit <watuka.t@gmail.com>
2016-04-06 15:30:41 +0700

    update test.bat

--- test.bat
+++ test.bat
@@ -1,10 +1,8 @@
 @echo off
-
 Packages\xunit.runner.console.2.1.0\tools\xunit.console ^
 	CarFuel.Facts\bin\Debug\CarFuel.Facts.dll ^
 	-parallel all ^
     -verbose ^
 	-html Result.html ^
 	-nologo
-	
 @echo on 
